550ccaa18ff7fa1aa67ad65845023f98049a9bb8
Erwin Korsten <mw.korsten@gmail.com>
2019-03-13 16:00:09 +0100

    CHANGE: User.php, config/database.php | USERSTORY: As user I want see my name if logged in

--- config/database.php
+++ config/database.php
@@ -69,7 +69,7 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
-            'schema' => 'public',
+            'schema' => env('DB_PGSQL_SCHEMA','public'),
             'sslmode' => 'prefer',
         ],
 
