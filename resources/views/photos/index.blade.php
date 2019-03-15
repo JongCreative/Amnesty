bf9086d539e2faf341e321baff6cc52df9faf52f
JongCreative <dci.jong@hotmail.com>
2019-03-15 12:31:53 +0100

    CHANGE: Photos.index  | USERSTORY: As user I want to see a link to the upload page

--- resources/views/photos/index.blade.php
+++ resources/views/photos/index.blade.php
@@ -2,8 +2,14 @@
 <h1>photos.index</h1>
 <p> all photos</p>
 
+<a href="/photos/create">upload your own photo</a>
+
+<br /><p>
+
 @foreach($photos as $photo)
-    <a href="/photos/{{ $photo->id }}">{{ $photo->src }}</a>
+    {{ $photo->id }}
+    <a href="/photos/{{ $photo->id }}"><img src="/storage/1552561211/{{ $photo->src }}"/></a>
     <hr/>
 @endforeach
+</p>
 {{ $photos->links() }}
