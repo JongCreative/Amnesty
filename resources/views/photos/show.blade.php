a6aa7f9930cbe21bc8a3cab92e5c7753dee8c10a
JongCreative <dci.jong@hotmail.com>
2019-03-19 12:57:58 +0100

    CHANGE: Photos.show | USERSTORY: As user I want to see a photo on the details page

--- resources/views/photos/show.blade.php
+++ resources/views/photos/show.blade.php
@@ -1,7 +1,7 @@
 <h1>photos.show</h1>
 <p> individual photos</p>
 <br/ > {{ $photos->id }}
-<br/ > {{ $photos->src }}
+<br/ > <img src="/storage/1552561211/{{ $photos->src }}"/>
 <br/ > {{ $photos->title }}
 <br/ > {{ $photos->descr }}
 
