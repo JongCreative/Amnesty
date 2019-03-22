67f7cb2353d012b880a3827f95a86778428e8273
Erwin Korsten <mw.korsten@gmail.com>
2019-03-22 11:01:23 +0100

    CHANGE: user.dashboard
    | USERSTORY: As user I want a good looking layout on the dashboard.

--- resources/views/users/dashboard.blade.php
+++ resources/views/users/dashboard.blade.php
@@ -1,55 +1,58 @@
-@extends('layouts.app')
+@extends('layouts.dev')
 
 @section('content')
-<div class="">
-    <div class="">Dashboard</div>
+<div class="forms_container" style="display: flex; justify-content: center; width: 100%; margin-top: 10px;">
+    <div class="form_container" style="max-width: 1000px;">
+        <div class="header">Dashboard</div>
+        <div style="padding: 20px; background-color: #c7c2ba">
+            <div style="margin: 8px 0;">
+                Welcome {{ Auth::user()->name }} <a href="/dashboard/name" style="display: block;">Change name</a>
+            </div>
 
-    <div class="">
-        Welcome {{ Auth::user()->name }} <a href="/dashboard/name">Change</a>
-    </div>
+            @if ( Auth::user()->admin )
+                <div style="margin: 8px 0;">
+                    <a href="/admin">Admin Dashboard</a>
+                </div>
+            @endif
+            <a href="/photos/create">Submit Photo</a>
 
-    @if ( Auth::user()->admin )
-        <div>
-            <a href="/admin">Admin Dashboard</a>
-        </div>
-    @endif
-    <a href="/photos/create">Submit Photo</a>
+            {{-- Success Handlers for Dashboard changes --}}
+            @if (session('success')) 
+                <div style="margin: 8px 0;">
+                    {{ session('success') }}
+                </div>
+             @endif
 
-    {{-- Success Handlers for Dashboard changes --}}
-    @if (session('success')) 
-        <div class="">
-            {{ session('success') }}
-        </div>
-     @endif
+            <div style="margin: 8px 0;">
+                {{ Auth::user()->email }}
+                <a href="/dashboard/email" style="display: block;">Change email</a>
+            </div>
 
-    <div class="">
-        {{ Auth::user()->email }}
-        <a href="/dashboard/email">Change</a>
-    </div>
+            <div style="margin: 8px 0;">
+                <a href="/dashboard/password">Change Password</a>
+            </div>
 
-    <div class="">
-        <a href="/dashboard/password">Change Password</a>
-    </div>
+            <div style="margin: 8px 0;">
+                <div class="">About me</div>
+                <div class=>{!! nl2br(e(Auth::user()->descr )) !!}</div>
+                <a href="/dashboard/description">Edit</a>
+            </div>
 
-    <div class="">
-        <div class="">About me</div>
-        <div class=>{!! nl2br(e(Auth::user()->descr )) !!}</div>
-        <a href="/dashboard/description">Edit</a>
-    </div>
-
-    <div>
-        <h2>Current Contest</h2>
-        <div>
-            Contest region: {{ $contestData->region  }}
-        </div>
-        <div>
-            Contest prize: {{ $contestData->prize  }}
-        </div>
-        <div>
-            Contest theme: {{ $contestData->theme }}
-        </div>
-        <div>
-            Contest end date: {{ $contestData->endDate }}
+            <div style="margin: 8px 0;">
+                <h2>Current Contest</h2>
+                <div>
+                    Contest region: {{ $contestData->region  }}
+                </div>
+                <div>
+                    Contest prize: {{ $contestData->prize  }}
+                </div>
+                <div>
+                    Contest theme: {{ $contestData->theme }}
+                </div>
+                <div>
+                    Contest end date: {{ $contestData->endDate }}
+                </div>
+            </div>
         </div>
     </div>
 </div>
