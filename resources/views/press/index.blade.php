5158cb79821621794e6c521d892c5c678da454fd
Erwin Korsten <mw.korsten@gmail.com>
2019-03-22 11:40:40 +0100

    CHANGE: sponsors.index.blade
    As user I want a good looking sponsor layout

--- resources/views/press/index.blade.php
+++ resources/views/press/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="forms_container" style="display: flex; justify-content: center; width: 100%; margin-top: 10px;">
     <div class="form_container" style="max-width: 100%; min-width: 75%">
-        <div class="header">Users</div>
+        <div class="header">Press</div>
         <div style="padding: 20px; background-color: #c7c2ba">
 
             {{-- Success Handler --}}
