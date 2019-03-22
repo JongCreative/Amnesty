307df75c05f2220483f883b794e2830f8ed2c007
Erwin Korsten <mw.korsten@gmail.com>
2019-03-22 11:23:43 +0100

    CHANGE: admin/dashboard.blade
    | USERSTORY: As admin I want a good looking layout on the admin dashboard.

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,59 +1,58 @@
-@extends('layouts.app')
+@extends('layouts.dev')
 
 @section('content')
-<div class="">
-    <div class="">Admin Dashboard</div>
+<div class="forms_container" style="display: flex; justify-content: center; width: 100%; margin-top: 10px;">
+    <div class="form_container" style="max-width: 100%; min-width: 75%">
+        <div class="header">Dashboard</div>
+        <div style="padding: 20px; background-color: #c7c2ba">
+            <div style="margin: 8px 0;">
+                Welcome {{ Auth::user()->name }}
+            </div>
 
-    <div class="">
+            {{-- Success Handler --}}
+            @if (session('success')) 
+                <div class="success">
+                    {{ session('success') }}
+                </div>
+             @endif
 
-        Welcome {{ Auth::user()->name }}
-    </div>
+            {{-- Creation links --}}
+            <div style="margin: 8px 0;">
+                <div><a href="/sponsors/create">Add new Sponsor</a></div>
+                <div><a href="/press/create">Add new press article</a></div>
+                <div><a href="/users">List users</a></div>
+            </div>
 
-    {{-- Success Handler --}}
-    @if (session('success')) 
-        <div class="success">
-            {{ session('success') }}
+
+            {{-- Contest Data --}}
+            <div style="margin: 8px 0;">
+                <a href="/admin/contest/edit">change contest data</a>
+            </div>
+            <div>
+                <a href="/admin/contest/create">Start new Contest</a>
+            </div>
+
+            <div style="margin: 8px 0;">
+                <div style="font-size: 1.2em; font-width: 700;">
+                    Contest id: {{ $contestData->contest }}
+                </div>
+                <div>
+                    Contest region: {{ $contestData->region  }}
+                </div>
+                <div>
+                    Contest prize: {{ $contestData->prize  }}
+                </div>
+                <div>
+                    Contest theme: {{ $contestData->theme }}
+                </div>
+                <div>
+                    Contest end date: {{ $contestData->endDate }}
+                </div>
+            </div>
+
         </div>
-     @endif
-
-    {{-- Creation links --}}
-    <div>
-        <div>
-            <a href="/sponsors/create">Add new Sponsor</a>
-            <a href="/press/create">Add new press article</a>
-        </div>
-    </div>
-
-
-    {{-- Contest Data --}}
-    <div>
-        <a href="/admin/contest/edit">change contest data</a>
-    </div>
-    <div>
-        <a href="/admin/contest/create">Start new Contest</a>
-    </div>
-
-    <div>
-        <div>
-            Contest id: {{ $contestData->contest }}
-        </div>
-        <div>
-            Contest region: {{ $contestData->region  }}
-        </div>
-        <div>
-            Contest prize: {{ $contestData->prize  }}
-        </div>
-        <div>
-            Contest theme: {{ $contestData->theme }}
-        </div>
-        <div>
-            Contest end date: {{ $contestData->endDate }}
-        </div>
-    </div>
-
-    <div>
-        <a href="/users">List users</a>
     </div>
 </div>
+
 
 @endsection
