921c3b8ddfe0326243c435ddf572a421a8cd2892
Erwin Korsten <mw.korsten@gmail.com>
2019-03-22 11:43:49 +0100

    CHANGE: press.show
    As user I want a good looking single press layout

--- resources/views/press/show.blade.php
+++ resources/views/press/show.blade.php
@@ -1,28 +1,33 @@
-@extends('layouts.app')
+@extends('layouts.dev')
 
 @section('content')
-<div class="">
-		<a href="/press">Back</a>
+<div class="forms_container" style="display: flex; justify-content: center; width: 100%; margin-top: 10px;">
+    <div class="form_container" style="max-width: 100%; min-width: 75%">
+        <div class="header">{{ $press->title }}</div>
+        <div style="padding: 20px; background-color: #c7c2ba">
 
-    {{-- Success Handler --}}
-    @if (session('success')) 
-        <div class="success">
-            {{ session('success') }}
+            {{-- Success Handlers for User changes --}}
+            @if (session('success')) 
+                <div class="">
+                    {{ session('success') }}
+                </div>
+             @endif
+
+            <div class="">{{ $press->source }}</div>
+            <div class="">{{ date('d-m-Y', strtotime($press->date)) }}</div>
+            <div class="">https://{{ $press->link }}</div>
+
+            <div style="margin: 8px 0;">
+                <a href="/press/{{ $press->id }}/edit">Edit press article</a>
+
+                <form method="POST" action="/press/{{$press->id}}">
+                    @csrf
+                    @method('DELETE')
+                  	<input type="submit" value="DELETE" style="background: red; color: white; max-width: 300px">
+                </form>
+            </div>
         </div>
-     @endif
-
-    <div class="">{{ $press->title }}</div>
-    <div class="">{{ $press->source }}</div>
-    <div class="">{{ date('d-m-Y', strtotime($press->date)) }}</div>
-    <div class="">https://{{ $press->link }}</div>
-
-    <a href="/press/{{ $press->id }}/edit">Edit press article</a>
-
-    <form method="POST" action="/press/{{$press->id}}">
-        @csrf
-        @method('DELETE')
-      	<input type="submit" value="DELETE">
-    </form>
+    </div>
     
 </div>
 
