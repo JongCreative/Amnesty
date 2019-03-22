dde73710c6e0f2c2fd0ec8c44ddfc087da23c7f2
Erwin Korsten <mw.korsten@gmail.com>
2019-03-22 09:42:27 +0100

    CHANGE: sponsors.create
    | USERSTORY: As admin I want a good looking form layout on the create sponsor page

--- resources/views/sponsors/create.blade.php
+++ resources/views/sponsors/create.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
 
-<div class="forms_container">
-    <div class="form_container">
+<div class="forms_container" style="display: flex; justify-content: center; width: 100%">
+    <div class="form_container" style="max-width: 500px;">
         <div class="header">New Sponsor</div>
         <form method="POST" action="/sponsors">
             @csrf
@@ -14,15 +14,15 @@
                 </div>
             @endif
             <label for="name">Name</label>
-            <input id="name" type="text" name="name" value="{{ old('name') }}" autofocus>
+            <input id="name" type="text" name="name" placeholder="Name"value="{{ old('name') }}" autofocus>
 
             @if ($errors->has('contribution'))
                 <div class="error">
                     {{ $errors->first('contribution') }}
                 </div>
             @endif
-            <label for="contribution">Contribution</label>
-            <textarea name="contribution" placeholder="Contribution" maxlength="500">{{ old('contribution') }}</textarea>
+            <label for="contribution" style="display: block;">Contribution</label>
+            <textarea name="contribution" placeholder="Contribution" maxlength="500" style="width: 100%; padding: 12px 20px; margin: 8px 0; display: block; border-radius: 4px; box-sizing: border-box; min-height: 100px; ">{{ old('contribution') }}</textarea>
 
             
             <input type="submit" value="Add Sponsor">
