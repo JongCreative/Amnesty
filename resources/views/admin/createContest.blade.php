0b3a5fe4815e0bc4f9f9447b1766ab07bf16e71d
Erwin Korsten <mw.korsten@gmail.com>
2019-03-22 09:23:05 +0100

    CHANGE: createContest.blade
    USERSTORY: As Admin I want a good looking form layout on the create contest page.

--- resources/views/admin/createContest.blade.php
+++ resources/views/admin/createContest.blade.php
@@ -1,27 +1,32 @@
-@extends('layouts.app')
+@extends('layouts.dev')
 
 @section('content')
-  <form method="POST" action="/admin/contest">
-    @csrf
+<div class="forms_container" style="display: flex; justify-content: center; width: 100%">
+    <div class="form_container" style="max-width: 500px;">
+        <form method="POST" action="/admin/contest">
+            @csrf
 
-    <label for="warning" class="">WARNING, THIS WILL START A NEW CONTEST AND ENDS THE CURRENT ONE. </label>
+            <label for="warning" class="">WARNING, THIS WILL START A NEW CONTEST AND ENDS THE CURRENT ONE. </label>
 
-    <label for="region" class="">Region</label>
+            <label for="region" class="">Region</label>
 
-    <input id="region" type="text" class="" name="region" value="{{ $contestData->region }}" required autofocus>
+            <input id="region" type="text" class="" name="region" value="{{ $contestData->region }}" required autofocus>
 
-    <label for="prize" class="">Prize</label>
-    <input id="prize " type="text" class="" name="prize" value="{{ $contestData->prize }}" required>
+            <label for="prize" class="">Prize</label>
+            <input id="prize " type="text" class="" name="prize" value="{{ $contestData->prize }}" required>
 
-    <label for="theme" class="">Theme</label>
-    <input id="theme" type="text" class="" name="theme" value="{{ $contestData->theme }}" required>
+            <label for="theme" class="">Theme</label>
+            <input id="theme" type="text" class="" name="theme" value="{{ $contestData->theme }}" required>
 
-    <label for="endDate" class="">End Date</label>
-    <input id="endDate" type="date" class="" name="endDate" value="{{ $contestData->endDate }}" required>
+            <label for="endDate" class="">End Date</label>
+            <input id="endDate" type="date" class="" name="endDate" value="{{ $contestData->endDate }}" required>
 
-    <label for="warning" class="">WARNING, THIS WILL START A NEW CONTEST AND ENDS THE CURRENT ONE. </label>
-    
-    <button type="submit" class="">Start new contest</button>
-</form>
+            <label for="warning" class="">WARNING, THIS WILL START A NEW CONTEST AND ENDS THE CURRENT ONE. </label>
+            
+            <input type="submit" value="Start new contest">
+        </form>
+
+    </div>
+</div>
 
 @endsection
