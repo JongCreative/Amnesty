3d133ad42fb6e02f7a95f48f04d3909d7b75e24e
Erwin Korsten <mw.korsten@gmail.com>
2019-03-25 16:52:55 +0100

    CHANGE: Welcome.blade
    As user I want to see the price for the third place

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -133,8 +133,8 @@
                 </div>
                 <div class="block">
                   <div class="title">3e prijs</div>
-                  <div class="sub">Lorem ipsum dolor sit amet</div>
-                  <div class="txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>              
+                  <div class="sub">{{ $contestData->thirdPrize }}</div>
+                  <div class="txt">{!! nl2br(e($contestData->thirdDescr)) !!}</div>              
                   <div class="button">meer info</div>
                 </div>
           </div>
