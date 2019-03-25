ac0baffed4b43e6f986a4cf237ce7037d9ec5e29
Esjes <esmeralda160867@hotmail.com>
2019-03-25 17:07:24 +0100

    CHANGE | USERSTORY:As user I want to see the press link

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,7 +22,7 @@
                     <li><a href="/terms">Voorwaarden</a></li>
                     <li><a href="{{ route('jury') }}">Jury</a></li>
                     <li><a href="/sponsors">Sposoren</a></li>
-                    <li><a href="#">Pers</a></li>
+                    <li><a href="/press">Pers</a></li>
                 </ul></li></div>
  @if (Route::has('login'))
     @auth
