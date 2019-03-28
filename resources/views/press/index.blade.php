7ec36058e8b538f209401f9fe8c0046888356e89
JongCreative <dci.jong@hotmail.com>
2019-03-28 08:09:56 +0100

    REMOVE: Press.index | All inline styling

--- resources/views/press/index.blade.php
+++ resources/views/press/index.blade.php
@@ -1,44 +1,42 @@
 @extends('layouts.dev')
 
 @section('content')
-<div class="forms_container" style="display: flex; justify-content: center; width: 100%; margin-top: 10px;">
-    <div class="form_container" style="max-width: 100%; min-width: 75%">
+<div class="forms_container">
+    <div class="form_container">
         <div class="header">Press</div>
-        <div style="padding: 20px; background-color: #c7c2ba">
+        <div>
 
             {{-- Success Handler --}}
-            @if (session('success'))
+            {{-- @if (session('success')) --}}
             <div class="success">
                 {{ session('success') }}
             </div>
-            @endif
+            {{-- @endif --}}
 
-            @if (Auth::user()->admin)
+            {{-- @if (Auth::user()->admin) --}}
             <div>
                 <a href="/press/create">Add new press article</a>
             </div>
-            @endif
+            {{-- @endif --}}
 
             {{-- Show all sponsors --}}
             @foreach($press as $pressArticle)
-            <div style="display: inline-block; margin: 8px;">
-                <div class="">{{ $pressArticle->title }}</div>
-                <div class="">{{ $pressArticle->source }}</div>
-                <div class="">{{ date('d-m-Y', strtotime($pressArticle->date)) }}</div>
-                <div class="">{!! nl2br(e( $pressArticle->descr))!!}</div>
-                <div class=""><a href="https://{{ $pressArticle->link }}">View article</a></div>
-
-                @if (Auth::user()->admin)
-                <a href="press/{{ $pressArticle->id }}">Show single article</a>
-                @endif
-
+            <div>
+                {{ $pressArticle->title }}
+                {{ $pressArticle->source }}
+                {{ date('d-m-Y', strtotime($pressArticle->date)) }}
+                {!! nl2br(e( $pressArticle->descr))!!}
+                <a href="https://{{ $pressArticle->link }}">View article</a>
             </div>
+
+                {{-- @if (Auth::user()->admin) --}}
+                    <a href="press/{{ $pressArticle->id }}">Show single article</a>
+                {{-- @endif --}}
+
+
             @endforeach
 
             {{ $press->links() }}
-        </div>
-    </div>
-</div>
 
 
 @endsection
