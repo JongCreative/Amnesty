c46751f518e64d548f4eae3f942049b0cf12097c
JongCreative <dci.jong@hotmail.com>
2019-03-29 00:50:58 +0100

    ADD: Users.show | USERSTORY: As admin I want the layout of the single-user-details page, to match the rest of the website

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,63 +1,56 @@
-@extends('layouts.dev')
+@extends('layouts.app')
+
+@section('pagetitle', 'User details')
 
 @section('content')
-<div>
-    <div>
-        <div class="header">{{ $user->banned ? 'Banned User' : $user->name }}</div>
-        <div>
-
-            {{-- Success Handlers for User changes --}}
-            @if (session('success'))
-            <div>
-                {{ session('success') }}
-            </div>
-            @endif
-
-            <div>{{ $user->title }}</div>
-            <div>
-                <img src="/img/avatar/{{ $user->avatar }}" alt="{{ $user->name }}'s avatar" />
-            </div>
-            <form action="/users/{{ $user->id }}/avatar" method="POST">
-
-                {{ method_field('DELETE') }}
-                {{ csrf_field() }}
-
-                <input type="submit" class="button" value="Delete Avatar" />
-            </form>
-
-            <div>
-                {{ $user->email }}
-            </div>
-
-            @if ($user->jury)
-            <div>
-                Jury
-            </div>
-            @endif
-
-            @if ($user->admin)
-            <div>
-                Admin
-            </div>
-            @endif
-
-            <div>
+{{-- Success Handlers for User changes --}}
+@include('inc.messages')
+<a href="/users"> return to users </a>
+<article class="content_container">
+    <div class="content_positioning content_positioning_1-2">
+        <article class="content_wrapper">
+            <section class="content_sub_wrapper">
+                <p class="h1">{{ $user->banned ? 'Banned User' : $user->name }}</p>
+            </section>
+            <section class="content_sub_wrapper">
+                <div>{{ $user->title }}</div>
+                <div>{{ $user->email }}</div>
+                @if ($user->jury)
+                    <div>Jury</div>
+                @endif
+                @if ($user->admin)
+                    <div>Admin</div>
+                @endif
                 <div>About me</div>
                 <div class=>{!! nl2br(e($user->descr )) !!}</div>
-            </div>
-
-            <a href="/users/{{$user->id}}/edit">Edit user</a>
-            <form method="POST" action="/users/{{ $user->id }}">
-                @csrf
-                @method('DELETE')
-
-                <input type="submit" value="Ban user">
-            </form>
-            <a href="/users">Back</a>
-        </div>
-
+            </section>
+        </article>
+        <article class="content_wrapper flex_row">
+            <section class="content_sub_wrapper">
+                <a class="button button_act" href="/users/{{$user->id}}/edit">Edit user</a>
+            </section>
+            <section class="content_sub_wrapper">
+                <form class="form-insert" method="POST" action="/users/{{ $user->id }}">
+                    @method('DELETE')            
+                    @csrf
+                    <input class="button btn_delete" type="submit" value="Ban user">
+                </form>
+            </section>
+        </article>
     </div>
-
-</div>
-
+    <div class="content_positioning content_positioning_2-2">
+        <article class="content_wrapper">
+            <section class="content_sub_wrapper">
+                <img src="/img/avatar/{{ $user->avatar }}" alt="{{ $user->name }}'s avatar" />
+            </section>
+            <section class="content_sub_wrapper">
+                <form action="/users/{{ $user->id }}/avatar" method="POST">
+                    {{ method_field('DELETE') }}
+                    {{ csrf_field() }}
+                    <input type="submit" class="button" value="Delete Avatar" />
+                </form>
+            </section>
+        </article>
+    </div>
+</article>
 @endsection
