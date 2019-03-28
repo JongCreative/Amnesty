0f461339ea4a8d69b0739d689100ebde6c71cbe3
JongCreative <dci.jong@hotmail.com>
2019-03-28 11:19:46 +0100

    REMOVE: Admin.editContest | All inline styling

--- resources/views/admin/editContest.blade.php
+++ resources/views/admin/editContest.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.dev')
 
 @section('content')
-<div class="forms_container" style="display: flex; justify-content: center; width: 100%">
-    <div class="form_container" style="max-width: 500px;">
+<div class="forms_container">
+    <div class="form_container">
         <div class="header">Edit contest</div>
         <form method="POST" action="/admin/contest">
             @csrf
             {{ method_field('PATCH') }}
-            <label for="id" style="display: block; font-size: 1.2em; font-weight: 700; margin-bottom: 8px;">Contest id: {{ $contestData->contest }}</label>
+            <label for="id">Contest id: {{ $contestData->contest }}</label>
 
             <label for="region" class="">Region</label>
 
@@ -16,7 +16,7 @@
             <label for="theme" class="">Theme</label>
             <input id="theme" type="text" class="" name="theme" value="{{ $contestData->theme }}" required>
 
-            <label for="endDate" style="display: block;">End Date</label>
+            <label for="endDate">End Date</label>
             <input id="endDate" type="date" class="" name="endDate" value="{{ $contestData->endDate }}" required>
 
             <label for="mainPrize" class="">Main Prize</label>
