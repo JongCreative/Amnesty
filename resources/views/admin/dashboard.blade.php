ca65482ec9410e6857fe7550ef3acf6d19289c51
JongCreative <dci.jong@hotmail.com>
2019-03-28 12:17:36 +0100

    ADD: Admin.dashboard | USERSTORY: As admin I want the layout of the admin-dashboard page, to match the rest of the website

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,73 +1,66 @@
-@extends('layouts.dev')
+
+@extends('layouts.app')
+
+@section('pagetitle', 'Dashboard')
 
 @section('content')
-<div class="forms_container">
-    <div class="form_container">
-        <div class="header">Dashboard</div>
-        <div>
-            <div>
-                Welcome {{ Auth::user()->name }}
-            </div>
+<article class="content_container flex_column">
+    <div class="content_positioning flex_row">
+        <article class="content_wrapper form_container">
+            <section class="content_sub_wrapper">
+                <p class="header">Welcome {{ Auth::user()->name }}</p>
+            </section>
 
-            {{-- Success Handler --}}
-            @if (session('success'))
-            <div class="success">
-                {{ session('success') }}
-            </div>
-            @endif
+            @include('inc.messages'){{-- Success Handlers for User changes --}}
 
-            {{-- Creation links --}}
-            <div>
+            <section class="content_sub_wrapper">
                 <div><a href="/sponsors/create">Add new Sponsor</a></div>
                 <div><a href="/press/create">Add new press article</a></div>
                 <div><a href="/users">List users</a></div>
-            </div>
+            </section>
+            <section class="content_sub_wrapper">
+                <div><a href="/admin/contest/edit">change contest data</a></div>
+                <div><a href="/admin/contest/create">Start new Contest</a></div>
+            </section>
+            <section class="content_sub_wrapper">
+                <div><a href="/admin/contest/edit">change contest data</a></div>
+                <div><a href="/admin/contest/create">Start new Contest</a></div>
+            </section>
+        </article>
 
-
-            {{-- Contest Data --}}
-            <div>
-                <a href="/admin/contest/edit">change contest data</a>
-            </div>
-            <div>
-                <a href="/admin/contest/create">Start new Contest</a>
-            </div>
-
-            <div >
-                <div>
-                    Contest id: {{ $contestData->contest }}
-                </div>
-                <div>
-                    Contest region: {{ $contestData->region  }}
-                </div>
-                <div>
-                    Contest theme: {{ $contestData->theme }}
-                </div>
-                <div>
-                    Contest end date: {{ $contestData->endDate }}
-                </div>
-                <div>
-                    Main prize: {{ $contestData->mainPrize }}
-                    <div>
-                        {!! nl2br(e($contestData->mainDescr)) !!}
-                    </div>
-                </div>
-                <div>
-                    2nd place prize: {{ $contestData->secondPrize }}
-                    <div>
-                        {!! nl2br(e($contestData->secondDescr)) !!}
-                    </div>
-                </div>
-                <div>
-                    3rd place prize: {{ $contestData->thirdPrize }}
-                    <div>
-                        {!! nl2br(e($contestData->thirdDescr)) !!}
-                    </div>
-                </div>
-            </div>
-
-        </div>
+        <article class="content_wrapper form_container">
+            <section class="content_sub_wrapper">
+                <p class="header">Current contest details</p>
+            </section>
+            <section class="content_sub_wrapper">
+                <div>Contest id: {{ $contestData->contest }}</div>
+                <div>Contest region: {{ $contestData->region  }}</div>
+                <div>Contest theme: {{ $contestData->theme }}</div>
+                <div>Contest end date: {{ $contestData->endDate }}</div>
+            </section>
+        </article>
     </div>
-</div>
-
+    <div class="content_positioning">
+        <article class="content_wrapper form_container">
+            <section class="content_sub_wrapper">
+                <p class="header">prizes</p>
+            </section>
+            <section class="content_sub_wrapper">
+                <div>Main prize: {{ $contestData->mainPrize }}</div>
+                <div>{!! nl2br(e($contestData->mainDescr)) !!}</div>
+            </section>
+        </article>
+        <article class="content_wrapper form_container flex_row">
+            <section class="content_sub_wrapper">
+                <div>2nd place prize: {{ $contestData->secondPrize }}</div>
+                <div>{!! nl2br(e($contestData->secondDescr)) !!}</div>
+            </section>
+            <section class="content_sub_wrapper">
+                <div>3rd place prize: {{ $contestData->thirdPrize }}</div>
+                <div>{!! nl2br(e($contestData->thirdDescr)) !!}</div>
+            </section>
+        </article>
+    </div>
+</article>
 
 @endsection
