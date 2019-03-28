77642286794cd551867eb110927fc53273fd5c3a
Esjes <esmeralda160867@hotmail.com>
2019-03-28 16:25:58 +0100

    CHANGE: jury.blade.php | merged layout with data

--- resources/views/jury.blade.php
+++ resources/views/jury.blade.php
@@ -1,41 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="">
 
-    @foreach($juryMembers as $juryMember)
-        <a href="users/{{ $juryMember->id }}">
-            <div class="">{{ $juryMember->name }}</div>
-            <div class="">{!! nl2br(e($juryMember->descr)) !!}</div>
-        </a>
-    @endforeach
-    <div class="grid_jury">
-        <div class="jury_card">
-            <img src="{{url('/img/jury1.jpg')}}" alt="">
-            <div class="name">David Bowie</div>
-            <div class="title">Donateur</div>
-            <div class="description">Daisy is student bij Code Gorilla en is daar een master in Laravel gebleken. Deze skills samen met haar talent voor structureren heeft zij voor dit project in gezet</div>
-        </div>
-        <div class="jury_card">
-            <img src="{{url('/img/jury2.jpg')}}" alt="">
-            <div class="name">David Bowie</div>
-            <div class="title">Donateur</div>
-            <div class="description">Daisy is student bij Code Gorilla en is daar een master in Laravel gebleken. Deze skills samen met haar talent voor structureren heeft zij voor dit project in gezet</div>
-        </div>
-        <div class="jury_card">
-            <img src="{{url('/img/jury3.jpg')}}" alt="">
-            <div class="name">David Bowie</div>
-            <div class="title">Donateur</div>
-            <div class="description">Daisy is student bij Code Gorilla en is daar een master in Laravel gebleken. Deze skills samen met haar talent voor structureren heeft zij voor dit project in gezet</div>
-        </div>
-        <div class="jury_card">
-            <img src="{{url('/img/jury4.jpg')}}" alt="">
-            <div class="name">David Bowie</div>
-            <div class="title">Donateur</div>
-            <div class="description">Daisy is student bij Code Gorilla en is daar een master in Laravel gebleken. Deze skills samen met haar talent voor structureren heeft zij voor dit project in gezet</div>
-        </div>
+<div class="grid_jury">
+    <div class="jury_card">
+        @foreach($juryMembers as $juryMember)
+            <a href="users/{{ $juryMember->id }}">
+                <img src="{{url('/img/jury1.jpg')}}" alt="">
+                <div class="name">{{ $juryMember->name }}</div>
+                <div class="title">Donateur</div>
+                <div class="description">{!! nl2br(e($juryMember->descr)) !!}</div>
+            </a>
+        @endforeach
     </div>
-
 </div>
 
 @endsection
