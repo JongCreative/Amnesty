1f187e27d2b1feb308304a08e957f94171e91fde
JongCreative <dci.jong@hotmail.com>
2019-03-28 09:46:36 +0100

    ADD: Press.create | USERSTORY: As admin I want the layout of the press-add-article page, to match the rest of the website

--- resources/views/press/create.blade.php
+++ resources/views/press/create.blade.php
@@ -1,53 +1,75 @@
-@extends('layouts.dev')
+@extends('layouts.app')
+
+@section('pagetitle', 'Add a press article!')
 
 @section('content')
-
-<div class="forms_container">
-    <div class="form_container">
-        <div class="header">New Press Article</div>
-        <form method="POST" action="/press">
-            @csrf
-
-            @if ($errors->has('title'))
-            <div class="error">
-                {{ $errors->first('title') }}
-            </div>
-            @endif
-            <label for="title">Title</label>
-            <input id="title" type="text" name="title" value="{{ old('title') }}" autofocus>
-
-            @if ($errors->has('src'))
-            <div class="error">
-                {{ $errors->first('src') }}
-            </div>
-            @endif
-            <label for="src">Source Name</label>
-            <input id="src" type="text" name="src" value="{{ old('src') }}">
-
-            @if ($errors->has('link'))
-            <div class="error">
-                {{ $errors->first('link') }}
-            </div>
-            @endif
-            <label for="link">Link to article</label>
-            <input id="link" type="text" name="link" value="{{ old('link')  }}">
-
-            @if ($errors->has('date'))
-            <div class="error">
-                {{ $errors->first('date') }}
-            </div>
-            @endif
-            <label for="date">Publish Date</label>
-            <input id="date" type="date" name="date" value="{{ old('date') ? old('date') : date('Y-m-d') }}">
-
-            <label for="descr">Description</label>
-
-            <textarea name="descr" placeholder="Description" maxlength="500">{{ old('descr') }}</textarea>
-
-            <input type="submit" value="Add Press Article">
-        </form>
-
+<a href="/photos"> return to gallery </a>
+<article class="content_container">
+    <div class="content_positioning content_positioning_1-2">
+        <article class="content_wrapper form_container">
+            <section class="content_sub_wrapper">
+                <p class="header">upload</p>
+            </section>
+            <section class="content_sub_wrapper">
+                <form  class="form_wrapper form-insert" action="/photos" enctype="multipart/form-data" method="POST">
+                    @csrf
+                    <section>
+                        <div>
+                            @if ($errors->has('title'))
+                            <div class="error">
+                                {{ $errors->first('title') }}
+                            </div>
+                            @endif
+                            <label>Title<input id="title" type="text" name="title" value="{{ old('title') }}" autofocus></label>
+                        </div>
+                        <div>
+                            @if ($errors->has('src'))
+                            <div class="error">
+                                {{ $errors->first('src') }}
+                            </div>
+                            @endif
+                            <label>Source Name<input id="src" type="text" name="src" value="{{ old('src') }}"></label>
+                        </div>
+                        <div>
+                            @if ($errors->has('link'))
+                            <div class="error">
+                                {{ $errors->first('link') }}
+                            </div>
+                            @endif
+                            <label>Link to article<input id="link" type="text" name="link" value="{{ old('link')  }}"></label>
+                        </div>
+                        <div>
+                            @if ($errors->has('date'))
+                            <div class="error">
+                                {{ $errors->first('date') }}
+                            </div>
+                            @endif
+                            <label>Publish Date<input id="date" type="date" name="date" value="{{ old('date') ? old('date') : date('Y-m-d') }}"></label>
+                        </div>
+                        <div>
+                            <br />
+                            <label>Description<textarea name="descr" placeholder="Description" maxlength="500">{{ old('descr') }}</textarea></label>
+                        </div>
+                        <div>
+                            .
+                        </div>
+                    <hr />
+                    </section>
+                    <section class="flex_row">
+                        <input class="button button_act" type="submit">
+                    </section>
+                    <section>
+                        <input type="reset" name="reset">
+                        <a href="/press"> cancel </a>
+                    </section>
+                </form>
+            </section>
+        </article>
     </div>
-</div>
-
+    <div class="content_positioning content_positioning_2-2">
+        <article class="content_wrapper">
+            <section class="content_sub_wrapper"></section>
+        </article>
+    </div>
+</article>
 @endsection
