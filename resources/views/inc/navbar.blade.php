1fe8a12b1ebc4be8192f2929975483f9f6aacee6
JongCreative <dci.jong@hotmail.com>
2019-03-29 11:02:32 +0100

    CHANGE: Inc.nav | USERSTORY: As user I want to have a dashboard link when logged in (dropdown)

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -18,10 +18,12 @@
                 </ul>
             </li>
         </div>
-        @if (Route::has('login')) @auth
+{{--         @if (Route::has('login')) @auth
         <div>
             <li><a href="/dashboard" class="dashboard">Dashboard</a></li>
-            <li><a href="{{ route('logout') }}" class="logout" onclick="event.preventDefault();
+
+            <li><a class="logout" href="{{ route('logout') }}"
+                    onclick="event.preventDefault();
                     document.getElementById('logout-form').submit();">
                     {{ __('Logout') }}
                 </a>
@@ -29,15 +31,40 @@
                     @csrf
                 </form>
             </li>
-        </div>
-        @else
-        <div class>
-            <li><a href="{{ route('login') }}" class="login">login</a></li>
 
-            @if (Route::has('register'))
-            <li><a href="{{ route('register') }} " class="register">Register</a></li>
-            @endif
         </div>
-        @endauth @endif
+
+        @else
+        <div> --}}
+            <!-- Authentication Links -->
+            @guest
+                <li><a class="login" href="{{ route('login') }}">{{ __('Login') }}</a></li>
+                @if (Route::has('register'))
+                    <li><a class="register" href="{{ route('register') }}">{{ __('Register') }}</a></li>
+                @endif
+            @else
+                <li>
+                    <li>{{Auth::user()->name}}
+                        <ul class="nav_child">
+                            <li><a href="/dashboard">Dashboard</a></li>
+                            <li><a href="/dashboard">Dashboard</a></li>
+                        </ul>
+                    <li><a class="logout" href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                        document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}</a>
+                    </li>
+                        
+                    <ul class="nav_child">
+                        <li>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                            </form>
+                        </li>
+                    </ul>
+                </li>
+                @endguest
+        </div>
+
     </ul>
 </nav>
