551644ed6a8d30cf4b91bd8b1fcab74061fc0d93
JongCreative <dci.jong@hotmail.com>
2019-03-29 01:25:04 +0100

    REMOVE: Users.index | All inline styling

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,19 +1,17 @@
 @extends('layouts.dev')
 
 @section('content')
-<div class="forms_container" style="display: flex; justify-content: center; width: 100%; margin-top: 10px;">
-	<div class="form_container" style="max-width: 100%; min-width: 75%">
-		<div class="header">Users</div>
-		<div style="padding: 20px; background-color: #c7c2ba">
+<div>
+	<div>
+		<div class="h1">Users</div>
+		<div>
 
 			@foreach($users as $user)
-			<a href="users/{{ $user->id }}" style="display: block;">
-				<div style="display: inline-block; margin: 8px;">{{ $user->name }}</div>
+			<a href="users/{{ $user->id }}" style="">
+				<div>{{ $user->name }}</div>
 				<div>{{ $user->title }}</div>
-				<div>
-					<img src="/img/avatar/{{ $user->avatar }}" alt="{{ $user->name }}'s avatar" />
-				</div>
-				<div style="display: inline-block; margin: 8px;">{{ $user->email }}</div>
+				<div><img src="/img/avatar/{{ $user->avatar }}" alt="{{ $user->name }}'s avatar" /></div>
+				<div>{{ $user->email }}</div>
 			</a>
 			@endforeach
 
