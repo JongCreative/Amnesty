7933ee6b1f156061d33900c8a0c42c8bf9720c5b
JongCreative <dci.jong@hotmail.com>
2019-03-29 02:31:09 +0100

    REMOVE: Users.changePassword | All inline styling

--- resources/views/users/changePassword.blade.php
+++ resources/views/users/changePassword.blade.php
@@ -2,8 +2,6 @@
 
 @section('content')
 
-<div class="forms_container" style="display: flex; justify-content: center; width: 100%">
-    <div class="form_container" style="max-width: 500px;">
         <div class="header">Change password</div>  
   
         <form method="POST" action="/dashboard/password">
@@ -17,13 +15,13 @@
              @endif
             
             <label>Current Password</label>
-            <input type="password" name="currentPassword" required style="width: 100%; padding: 12px 20px; margin: 8px 0; display: inline-block; border: 1px solid #ccc; border-radius: 4px; box-sizing: border-box;"/>
+            <input type="password" name="currentPassword" required>
                 
             <label>New Password</label>
-            <input type="password" name="newPassword" required style="width: 100%; padding: 12px 20px; margin: 8px 0; display: inline-block; border: 1px solid #ccc; border-radius: 4px; box-sizing: border-box;"/>
+            <input type="password" name="newPassword" required>
 
             <label>Confirm New Password</label>
-            <input type="password" name="newPassword_confirmation" required style="width: 100%; padding: 12px 20px; margin: 8px 0; display: inline-block; border: 1px solid #ccc; border-radius: 4px; box-sizing: border-box;"/>
+            <input type="password" name="newPassword_confirmation" required>
 
             <input type="submit" value="Change Password">
             <a href="/dashboard" class="button?"> Back </a>
