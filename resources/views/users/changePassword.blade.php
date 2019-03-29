7c5ea3d521398eaaf9631ac428cfba227e5c9fc8
JongCreative <dci.jong@hotmail.com>
2019-03-29 02:35:39 +0100

    ADD: Users.changePassword | USERSTORY: As admin I want the layout of the change-password page, to match the rest of the website

--- resources/views/users/changePassword.blade.php
+++ resources/views/users/changePassword.blade.php
@@ -1,32 +1,41 @@
-@extends('layouts.dev')
+@extends('layouts.app')
+
+@section('pagetitle', 'Change password')
 
 @section('content')
-
-        <div class="header">Change password</div>  
-  
-        <form method="POST" action="/dashboard/password">
-            @csrf
-            @method('PATCH')
-
-            @if (session('error')) 
-                <div class="">
-                    {{ session('error') }}
-                </div>
-             @endif
-            
-            <label>Current Password</label>
-            <input type="password" name="currentPassword" required>
-                
-            <label>New Password</label>
-            <input type="password" name="newPassword" required>
-
-            <label>Confirm New Password</label>
-            <input type="password" name="newPassword_confirmation" required>
-
-            <input type="submit" value="Change Password">
-            <a href="/dashboard" class="button?"> Back </a>
-        </form>
-
+<a href="/dashboard"> return to dashboard </a>
+<article class="content_container">
+    <div class="content_positioning content_positioning_1-2">
+        <article class="content_wrapper form_container flex_column">
+            <section class="content_sub_wrapper">
+                <div class="h1">Change About me</div>
+            </section>
+            <section class="content_sub_wrapper">
+                <form class="form_wrapper form-insert" method="POST" action="/dashboard/password">
+                    @csrf
+                    @method('PATCH')
+                    <section>
+                        {{-- Success Handlers for User changes --}}
+                        @include('inc.messages')
+                    </section>
+                    <section>
+                        <div><label>Current Password<input type="password" name="currentPassword" required></label></div>
+                        <div><label>New Password<input type="password" name="newPassword" required></label></div>
+                        <div><label>Confirm New Password<input type="password" name="newPassword_confirmation" required></label></div>
+                    </section>
+                    <section class="flex_row">
+                        <input class="button button_act" type="reset" name="reset">
+                        <input class="button button_act" type="submit" value="Change Password">
+                    </section>
+                </form>
+            </section>
+        </article>
     </div>
-</div>
+    <div class="content_positioning content_positioning_2-2">
+        <article class="content_wrapper">
+            <section class="content_sub_wrapper"></section>
+        </article>
+    </div>
+</article>
 @endsection
+
