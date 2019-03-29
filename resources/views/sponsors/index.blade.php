e628e56a7c7a1d4d00d471cd7bc197bdb75063bb
JongCreative <dci.jong@hotmail.com>
2019-03-29 12:25:10 +0100

    CHANGE: Sponsor.index | USERSTORY: As admin I want the layout of the show-all-sponsors page, to match the rest of the website

--- resources/views/sponsors/index.blade.php
+++ resources/views/sponsors/index.blade.php
@@ -1,88 +1,83 @@
 @extends('layouts.app')
 
-@section('pagetitle', 'Sponsoren')
+@section('pagetitle', 'All Sponsors')
 
 @section('content')
-
-            {{-- Success Handler --}}
-            @if (session('success'))
-                <div class="alert alert-success">
-                    {{ session('success') }}
-                </div>
-            @endif
-
-            {{-- Show all sponsors --}}
-            @foreach($sponsors as $sponsor)
-                <div>
-                    <a href="sponsors/{{ $sponsor->id }}"></a>
-                    {{ $sponsor->name }}
-                    <img src="/img/logo/{{ $sponsor->logo }}" alt="{{ $sponsor->name }}'s logo" />
-                    {{ $sponsor->title }} 
-                    {!! nl2br(e($sponsor->contribution)) !!}
-                </div>
-            @endforeach
-
-            {{ $sponsors->links() }}
-<div class="grid_sponsors">
-    <div class="sponsor_card">
-        <img src="{{url('/img/devlucht.jpg')}}" alt="">
-        <div class="name">Daisy Jong</div>
-        <div class="title">Backend, scrummaster</div>
-        <div class="description">Daisy is student bij Code Gorilla en is daar een master in Laravel gebleken. Deze skills samen met haar talent voor structureren heeft zij voor dit project in gezet</div>
-    </div>
-    <div class="sponsor_card">
-        <img src="{{url('/img/devlucht.jpg')}}" alt="">
-        <div class="name">Daisy Jong</div>
-        <div class="title">Backend, scrummaster</div>
-        <div class="description">Daisy is student bij Code Gorilla en is daar een master in Laravel gebleken. Deze skills samen met haar talent voor structureren heeft zij voor dit project in gezet</div>
-    </div>
-    <div class="sponsor_card">
-        <img src="{{url('/img/devlucht.jpg')}}" alt="">
-        <div class="name">Daisy Jong</div>
-        <div class="title">Backend, scrummaster</div>
-        <div class="description">Daisy is student bij Code Gorilla en is daar een master in Laravel gebleken. Deze skills samen met haar talent voor structureren heeft zij voor dit project in gezet</div>
-    </div>
-    <div class="sponsor_card">
-        <img src="{{url('/img/devlucht.jpg')}}" alt="">
-        <div class="name">Daisy Jong</div>
-        <div class="title">Backend, scrummaster</div>
-        <div class="description">Daisy is student bij Code Gorilla en is daar een master in Laravel gebleken. Deze skills samen met haar talent voor structureren heeft zij voor dit project in gezet</div>
-    </div>
-    <div class="sponsor_card">
-        <img src="{{url('/img/devlucht.jpg')}}" alt="">
-        <div class="name">Daisy Jong</div>
-        <div class="title">Backend, scrummaster</div>
-        <div class="description">Daisy is student bij Code Gorilla en is daar een master in Laravel gebleken. Deze skills samen met haar talent voor structureren heeft zij voor dit project in gezet</div>
-    </div>
-    <div class="sponsor_card">
-        <img src="{{url('/img/devlucht.jpg')}}" alt="">
-        <div class="name">Daisy Jong</div>
-        <div class="title">Backend, scrummaster</div>
-        <div class="description">Daisy is student bij Code Gorilla en is daar een master in Laravel gebleken. Deze skills samen met haar talent voor structureren heeft zij voor dit project in gezet</div>
-    </div>
-</div>
-@endsection
-
-{{-- @extends('layouts.app')
-
-@section('pagetitle', 'Join the contest!')
-
-@section('content')
-<article class="content_container">
-    <div class="content_positioning content_positioning_1-2">
-        <article class="content_wrapper form_container">
-            <section class="content_sub_wrapper"></section>
-            <section class="content_sub_wrapper"></section>
-        </article>
-        <article class="content_wrapper form_container">
-            <section class="content_sub_wrapper"></section>
-            <section class="content_sub_wrapper"></section>
-        </article>
-    </div>
-    <div class="content_positioning content_positioning_2-2">
+<article class="content_container flex_column">
+    <div class="content_positioning">
         <article class="content_wrapper">
-            <section class="content_sub_wrapper"></section>
+            {{-- Success Handlers for User changes --}}
+            @include('inc.messages')
+        </article>
+        @if (Auth::user()->admin)
+        <article class="content_wrapper">
+            <a href="/press/create">Add new press article</a>
+        </article>
+        @endif
+    </div>
+    <div class="content_positioning cards_container">
+    @foreach($sponsors as $sponsor)
+        <article class="content_wrapper flex_column card_wrapper">
+            <section class="content_sub_wrapper">
+                <p class="h1">{{ $sponsor->name }}</p>
+            </section>
+            <section class="content_sub_wrapper">
+                <div><a href="sponsors/{{ $sponsor->id }}"></a></div>
+                <div><img src="/img/logo/{{ $sponsor->logo }}" alt="{{ $sponsor->name }}'s logo" /></div>
+                <div>{{ $sponsor->title }} </div>
+                <div>{!! nl2br(e($sponsor->contribution)) !!}</div>
+            </section>
+            @if (Auth::user()->admin)
+                <section class="content_sub_wrapper">
+                    <div><a href="sponsors/{{ $sponsor->id }}">Show single sponsor</a></div>
+                </section>
+            @endif
+        </article>
+    @endforeach
+    </div>
+    <div class="content_positioning">
+        <article class="content_wrapper flex_row">
+        {{ $sponsors->links() }}
         </article>
     </div>
 </article>
-@endsection --}}
+@endsection
+{{-- 
+<div class="grid_sponsors">
+        <div class="sponsor_card">
+            <img src="{{url('/img/devlucht.jpg')}}" alt="">
+            <div class="name">Daisy Jong</div>
+            <div class="title">Backend, scrummaster</div>
+            <div class="description">Daisy is student bij Code Gorilla en is daar een master in Laravel gebleken. Deze skills samen met haar talent voor structureren heeft zij voor dit project in gezet</div>
+        </div>
+        <div class="sponsor_card">
+            <img src="{{url('/img/devlucht.jpg')}}" alt="">
+            <div class="name">Daisy Jong</div>
+            <div class="title">Backend, scrummaster</div>
+            <div class="description">Daisy is student bij Code Gorilla en is daar een master in Laravel gebleken. Deze skills samen met haar talent voor structureren heeft zij voor dit project in gezet</div>
+        </div>
+        <div class="sponsor_card">
+            <img src="{{url('/img/devlucht.jpg')}}" alt="">
+            <div class="name">Daisy Jong</div>
+            <div class="title">Backend, scrummaster</div>
+            <div class="description">Daisy is student bij Code Gorilla en is daar een master in Laravel gebleken. Deze skills samen met haar talent voor structureren heeft zij voor dit project in gezet</div>
+        </div>
+        <div class="sponsor_card">
+            <img src="{{url('/img/devlucht.jpg')}}" alt="">
+            <div class="name">Daisy Jong</div>
+            <div class="title">Backend, scrummaster</div>
+            <div class="description">Daisy is student bij Code Gorilla en is daar een master in Laravel gebleken. Deze skills samen met haar talent voor structureren heeft zij voor dit project in gezet</div>
+        </div>
+        <div class="sponsor_card">
+            <img src="{{url('/img/devlucht.jpg')}}" alt="">
+            <div class="name">Daisy Jong</div>
+            <div class="title">Backend, scrummaster</div>
+            <div class="description">Daisy is student bij Code Gorilla en is daar een master in Laravel gebleken. Deze skills samen met haar talent voor structureren heeft zij voor dit project in gezet</div>
+        </div>
+        <div class="sponsor_card">
+            <img src="{{url('/img/devlucht.jpg')}}" alt="">
+            <div class="name">Daisy Jong</div>
+            <div class="title">Backend, scrummaster</div>
+            <div class="description">Daisy is student bij Code Gorilla en is daar een master in Laravel gebleken. Deze skills samen met haar talent voor structureren heeft zij voor dit project in gezet</div>
+        </div>
+    </div> --}}
