bb4c61753eac4c031089bbc4b332cb49c7bf4fdb
JongCreative <dci.jong@hotmail.com>
2019-03-29 02:11:02 +0100

    ADD: Users.changeDescription | USERSTORY: As admin I want the layout of the change-users-description page, to match the rest of the website

--- resources/views/users/changeDescription.blade.php
+++ resources/views/users/changeDescription.blade.php
@@ -1,31 +1,40 @@
-@extends('layouts.dev')
+@extends('layouts.app')
+
+@section('pagetitle', 'Change About me')
 
 @section('content')
-
-<div class="forms_container">
-    <div class="form_container">
-        <div class="header">Change About me</div>  
-  
-        <form method="POST" action="/dashboard/description">
-            @csrf
-            @method('PATCH')
-
-            @if (session('error')) 
-                <div class="">
-                    {{ session('error') }}
-                </div>
-            @endif
-              
-            <label for="descr" class=""> About me </label>
-            <textarea name="descr" placeholder="Description" maxlength="500">{{ Auth::user()->descr }}</textarea>
-
-            <label for="currentPassword" class=""> Password </label>
-
-            <input id="currentPassword" type="password" class="" name="currentPassword" required>
-
-            <input type="submit" value="Update About Me">
-            <a href="/dashboard" class="button?"> Back </a>
-        </form>
+<a href="/dashboard"> return to dashboard </a>
+<article class="content_container">
+    <div class="content_positioning content_positioning_1-2">
+        <article class="content_wrapper form_container flex_column">
+            <section class="content_sub_wrapper">
+                <div class="h1">Change About me</div>
+            </section>
+            <section class="content_sub_wrapper">
+                <form class="form_wrapper form-insert" method="POST" action="/dashboard/description">
+                    @csrf
+                    @method('PATCH')
+                    <section>
+                        {{-- Success Handlers for User changes --}}
+                        @include('inc.messages')
+                    </section>
+                    <section>
+                        <label>About me<textarea name="descr" placeholder="Description" maxlength="500">{{ Auth::user()->descr }}</textarea></label>
+                        <label>Password<input id="currentPassword" type="password" class="" name="currentPassword" required></label>
+                    </section>
+                    <section>
+                        <input class="btn" type="reset" name="reset">
+                        <input type="submit" value="Update About Me">
+                    </section>
+                </form>
+            </section>
+        </article>
     </div>
-</div>
+    <div class="content_positioning content_positioning_2-2">
+        <article class="content_wrapper">
+            <section class="content_sub_wrapper"></section>
+        </article>
+    </div>
+</article>
 @endsection
+
