cc77283261082f4ea7fe2ed9201d981f2bafa960
JongCreative <dci.jong@hotmail.com>
2019-03-29 01:36:06 +0100

    ADD: Users.index | USERSTORY: As admin I want the layout of the view-all-users-details page, to match the rest of the website

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,24 +1,35 @@
-@extends('layouts.dev')
+@extends('layouts.app')
+
+@section('pagetitle', 'All users page')
 
 @section('content')
-<div>
-	<div>
-		<div class="h1">Users</div>
-		<div>
-
-			@foreach($users as $user)
-			<a href="users/{{ $user->id }}" style="">
-				<div>{{ $user->name }}</div>
-				<div>{{ $user->title }}</div>
-				<div><img src="/img/avatar/{{ $user->avatar }}" alt="{{ $user->name }}'s avatar" /></div>
-				<div>{{ $user->email }}</div>
+<article class="content_container flex_column">
+    <div class="content_positioning">
+        <article class="content_wrapper">
+            {{-- Success Handlers for User changes --}}
+            @include('inc.messages')
+        </article>
+    </div>
+    <div class="content_positioning cards_container">
+    @foreach($users as $user)
+        <article class="content_wrapper flex_column card_wrapper">
+			<a href="users/{{ $user->id }}">
+				<section class="content_sub_wrapper">
+					<p class="h1">{{ $user->name }}</p>
+				</section>
+				<section class="content_sub_wrapper">
+					<div>{{ $user->title }}</div>
+					<div><img class="img_size" src="/img/avatar/{{ $user->avatar }}" alt="{{ $user->name }}'s avatar" /></div>
+					<div>{{ $user->email }}</div>
+				</section>
 			</a>
-			@endforeach
-
+        </article>
+    @endforeach
+    </div>
+    <div class="content_positioning">
+        <article class="content_wrapper flex_row">
 			{{ $users->links() }}
-		</div>
-	</div>
-</div>
-
-
+        </article>
+    </div>
+</article>
 @endsection
