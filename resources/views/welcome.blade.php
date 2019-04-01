1ed7fa264ccaab036c3a492b31433ecda672edf6
JongCreative <dci.jong@hotmail.com>
2019-04-01 09:29:05 +0200

    CHANGE: Welcome.blade.php | USERSTORY: As user i want to be able to navigate to the join the contest page from the landings page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,7 +27,7 @@
             <div class="title">Hoe ziet vluchten er voor jou uit?</div><br>
             <p>Deze eerste editie 2019 heeft als thema "vluchten". Met mooie prijzen en maandelijkse winnaars. Iederereen mag mee doen. Van professionals tot hobbyisten. </p>
             <p>De sluitingsdatum voor de hoofdprijs is {{ $contestData->endDate }}</p>
-            <div class="button">Doe mee!</div>
+            <div class="button"><a href="/photos">Doe mee!</a></div>
           </div>
         </div>
       </div>
@@ -45,19 +45,19 @@
               <div class="title">De wedstrijd</div>
               <div class="sub">Lorem ipsum dolor sit amet</div>
               <div class="txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
-              <div class="button">Deelnemen</div>
+              <div class="button"><a href="/photos/create">Deelnemen</a></div>
             </div>
             <div class="block">
               <div class="title">Series</div>
               <div class="sub">Lorem ipsum dolor sit amet</div>
               <div class="txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
-              <div class="button">Deelnemen</div>
+              <div class="button"><a href="/photos/create">Deelnemen</a></div>
             </div>
             <div class="block">
               <div class="title">Enkele foto's</div>
               <div class="sub">Lorem ipsum dolor sit amet</div>
               <div class="txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
-              <div class="button">Deelnemen</div>
+              <div class="button"><a href="/photos/create">Deelnemen</a></div>
             </div>
           </div>
           <div class="textheader">
@@ -65,7 +65,7 @@
               <div class="sub">{{ $contestData->currentPrize }}</div>
             </div>
             <div class="txt">{{ $contestData->monthDescr }}</div>
-            <div class="button">Deelnemen</div>
+            <div class="button"><a href="/photos/create">Deelnemen</a></div>
           </div>
           <!-- end content parallax -->
         </div>
@@ -91,19 +91,19 @@
               <div class="title">Hoofdprijs</div>
               <div class="sub">{{ $contestData->mainPrize }}</div>
               <div class="txt">{!! nl2br(e($contestData->mainDescr)) !!}</div>
-              <div class="button">meer info</div>
+              <div class="button"><a href="#">meer info</a></div>
             </div>
             <div class="block">
               <div class="title">2e prijs</div>
               <div class="sub">{{ $contestData->secondPrize }}</div>
               <div class="txt">{!! nl2br(e($contestData->secondDescr)) !!}</div>
-              <div class="button">meer info</div>
+              <div class="button"><a href="#">meer info</a></div>
             </div>
             <div class="block">
               <div class="title">3e prijs</div>
               <div class="sub">{{ $contestData->thirdPrize }}</div>
               <div class="txt">{!! nl2br(e($contestData->thirdDescr)) !!}</div>
-              <div class="button">meer info</div>
+              <div class="button"><a href="#">meer info</a></div>
             </div>
           </div>
         </div>
@@ -112,7 +112,7 @@
             <div class="sub">Elke maand een stedentrip</div>
           </div>
           <div class="txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magn</div>
-          <div class="button">Venetie</div>
+          <div class="button"><a href="#">Venetie</a></div>
         </div>
         <!-- end content parallax -->
       </div>
