8ccfeff4d5d73c7dbe10087168c51c3bf3fe060e
Erwin Korsten <mw.korsten@gmail.com>
2019-04-04 10:57:11 +0200

    FIX: Mobile navigation
    Now mobile navigation has decent links.

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -8,7 +8,7 @@
         <ul class="nav_child">
           <li><a href="/terms">Voorwaarden</a></li>
           <li><a href="/jury">Jury</a></li>
-          <li><a href="/sponsors">Sposoren</a></li>
+          <li><a href="/sponsors">Sponsoren</a></li>
           <li><a href="/press">Pers</a></li>
         </ul>
       </li>
@@ -17,31 +17,66 @@
     <div class="nav_screen">
       <!-- Authentication Links -->
       @guest
-        <li><a class="login" href="{{ route('login') }}">{{ __('Login') }}</a></li>
-        @if (Route::has('register'))
-          <li><a class="register" href="{{ route('register') }}">{{ __('Register') }}</a></li>
-        @endif
+      <li><a class="login" href="{{ route('login') }}">{{ __('Login') }}</a></li>
+      @if (Route::has('register'))
+      <li><a class="register" href="{{ route('register') }}">{{ __('Register') }}</a></li>
+      @endif
       @else
-        <li>
-        <li>{{Auth::user()->name}}
-          <ul class="nav_child">
-            <li><a href="/dashboard">Dashboard</a></li>
-            <li><a href="/photos/create">upload photo</a></li>
-          </ul>
-        <li><a class="logout" href="{{ route('logout') }}" onclick="event.preventDefault();
-                          document.getElementById('logout-form').submit();">
-            {{ __('Logout') }}</a>
-        </li>
-
+      <li>
+      <li>{{Auth::user()->name}}
         <ul class="nav_child">
-          <li>
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-              @csrf
-            </form>
-          </li>
+          <li><a href="/dashboard">Dashboard</a></li>
+          <li><a href="/photos/create">upload photo</a></li>
         </ul>
+      <li><a class="logout" href="{{ route('logout') }}" onclick="event.preventDefault();
+                          document.getElementById('logout-form').submit();">
+          {{ __('Logout') }}</a>
+      </li>
+
+      <ul class="nav_child">
+        <li>
+          <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+            @csrf
+          </form>
         </li>
+      </ul>
+      </li>
       @endguest
     </div>
+
+    {{-- Mobile only (Different to keep windows layout) --}}
+
+    <div class="nav_mobile">
+      <input type="checkbox" class="nav_toggle" id="nav_toggle">
+      <li class="menu_button"><label class="menu_button" for="nav_toggle"><span></span></label></li>
+
+      <ul class="nav_links">
+        <li><a href="/">Home</a></li>
+        <li><a href="/photos">Gallery</a></li>
+        <li><a href="/terms">Voorwaarden</a></li>
+        <li><a href="/jury">Jury</a></li>
+        <li><a href="/sponsors">Sponsoren</a></li>
+        <li><a href="/press">Pers</a></li>
+        <!-- Authentication Links -->
+        @guest
+          <li><a href="{{ route('login') }}">{{ __('Login') }}</a></li>
+          @if (Route::has('register'))
+            <li><a href="{{ route('register') }}">{{ __('Register') }}</a></li>
+          @endif
+        @else
+          <li><a href="/dashboard">Dashboard</a></li>
+          <li><a href="/photos/create">upload photo</a></li>
+          <li><a href="{{ route('logout') }}" onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();">
+              {{ __('Logout') }}</a>
+              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+              </form>
+          </li>
+        @endguest
+      </ul>
+
+    </div>
   </ul>
+
 </nav>
