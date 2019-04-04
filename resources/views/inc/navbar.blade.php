f98b3f57964ace865726fb075e176c8228e2dbf2
Erwin Korsten <mw.korsten@gmail.com>
2019-04-04 13:43:45 +0200

    FIX: Navbar once more
    One bug after another is getting found in the navbar.

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -7,7 +7,7 @@
       <li><a href="/photos">Gallery</a></li>
       <li>Wedstrijd
         <ul class="nav_child">
-          <li><a href="/terms">Voorwaarden</a></li>
+          {{-- <li><a href="/terms">Voorwaarden</a></li> --}}
           <li><a href="/jury">Jury</a></li>
           <li><a href="/sponsors">Sponsoren</a></li>
           <li><a href="/press">Pers</a></li>
@@ -18,30 +18,24 @@
     <div class="nav_screen">
       <!-- Authentication Links -->
       @guest
-      <li><a class="login" href="{{ route('login') }}">{{ __('Login') }}</a></li>
-      @if (Route::has('register'))
-      <li><a class="register" href="{{ route('register') }}">{{ __('Register') }}</a></li>
-      @endif
+        <li><a class="login" href="{{ route('login') }}">{{ __('Login') }}</a></li>
+        @if (Route::has('register'))
+        <li><a class="register" href="{{ route('register') }}">{{ __('Register') }}</a></li>
+        @endif
       @else
-      <li>
-      <li>{{Auth::user()->name}}
-        <ul class="nav_child">
-          <li><a href="/dashboard">Dashboard</a></li>
-          <li><a href="/photos/create">upload photo</a></li>
-        </ul>
-      <li><a class="logout" href="{{ route('logout') }}" onclick="event.preventDefault();
-                          document.getElementById('logout-form').submit();">
-          {{ __('Logout') }}</a>
-      </li>
-
-      <ul class="nav_child">
-        <li>
-          <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-            @csrf
-          </form>
+        <li>{{Auth::user()->name}}
+          <ul class="nav_child">
+            <li><a href="/dashboard">Dashboard</a></li>
+            <li><a href="/photos/create">upload photo</a></li>
+          </ul>
         </li>
-      </ul>
-      </li>
+        <li><a class="logout" href="{{ route('logout') }}" onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();">
+            {{ __('Logout') }}</a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+              @csrf
+            </form>
+        </li>
       @endguest
     </div>
 
@@ -54,7 +48,7 @@
       <ul class="nav_links">
         <li><a href="/">Home</a></li>
         <li><a href="/photos">Gallery</a></li>
-        <li><a href="/terms">Voorwaarden</a></li>
+        {{-- <li><a href="/terms">Voorwaarden</a></li> --}}
         <li><a href="/jury">Jury</a></li>
         <li><a href="/sponsors">Sponsoren</a></li>
         <li><a href="/press">Pers</a></li>
