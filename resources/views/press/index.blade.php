d98e4050cabfb684a59162ccfc6d425818018980
Esjes <esmeralda160867@hotmail.com>
2019-04-09 15:55:56 +0200

    CHANGE: press.scss, press/index | USERSTORY: As user I want more whitespace on the press page

--- resources/views/press/index.blade.php
+++ resources/views/press/index.blade.php
@@ -3,10 +3,43 @@
 @section('pagetitle', 'All press articles')
 
 @section('content')
+<div class="press_container">
+    <div class="press_card">
+        <div class="card_name">Amnesty organiseert eerste editie photwedstrijd</div>  
+        <div class="card_date">15 maart 2020</div>
+        <div class="card_description">Pietjes Puk is al 25 jaar beeldredacteur bij het NRC. Pietje heeft heel veel verstand van foto's. In 1530 was hij de eerste winnaar van de zilveren camera</div>
+        <div class="card_link">naar het artikel van het AD.nl</div> 
+    </div>
+    <div class="press_card">
+        <div class="card_name">Amnesty organiseert eerste editie photwedstrijd</div>  
+        <div class="card_date">15 maart 2020</div>
+        <div class="card_description">Pietjes Puk is al 25 jaar beeldredacteur bij het NRC. Pietje heeft heel veel verstand van foto's. In 1530 was hij de eerste winnaar van de zilveren camera</div>
+        <div class="card_link">naar het artikel van het AD.nl</div> 
+    </div>
+    <div class="press_card">
+        <div class="card_name">Amnesty organiseert eerste editie photwedstrijd</div>  
+        <div class="card_date">15 maart 2020</div>
+        <div class="card_description">Pietjes Puk is al 25 jaar beeldredacteur bij het NRC. Pietje heeft heel veel verstand van foto's. In 1530 was hij de eerste winnaar van de zilveren camera</div>
+        <div class="card_link">naar het artikel van het AD.nl</div> 
+    </div>
+    <div class="press_card">
+        <div class="card_name">Amnesty organiseert eerste editie photwedstrijd</div>  
+        <div class="card_date">15 maart 2020</div>
+        <div class="card_description">Pietjes Puk is al 25 jaar beeldredacteur bij het NRC. Pietje heeft heel veel verstand van foto's. In 1530 was hij de eerste winnaar van de zilveren camera</div>
+        <div class="card_link">naar het artikel van het AD.nl</div> 
+    </div>
+    <div class="press_card">
+        <div class="card_name">Amnesty organiseert eerste editie photwedstrijd</div>  
+        <div class="card_date">15 maart 2020</div>
+        <div class="card_description">Pietjes Puk is al 25 jaar beeldredacteur bij het NRC. Pietje heeft heel veel verstand van foto's. In 1530 was hij de eerste winnaar van de zilveren camera</div>
+        <div class="card_link">naar het artikel van het AD.nl</div> 
+    </div>
+</div>
+{{-- 
 <article class="content_container flex_column">
     <div class="content_positioning">
         <article class="content_wrapper">
-            {{-- Success Handlers for User changes --}}
+            {{-- Success Handlers for User changes 
             @include('inc.messages')
         </article>
         @guest
@@ -23,7 +56,7 @@
     <article class="content_wrapper flex_column card_wrapper">
         <section class="content_sub_wrapper card_sub_wrapper flex_wrap">
             <div class="flex_1 center_content">
-                {{-- <div><img src="/img/logo/{{ $pressArticle->source }}" alt="{{ $pressArticle->source }}'s logo" /></div> --}}
+                {{-- <div><img src="/img/logo/{{ $pressArticle->source }}" alt="{{ $pressArticle->source }}'s logo" /></div> 
                 <div><img src="https://pbs.twimg.com/profile_images/931190512731807746/yO8Lcr9H_400x400.jpg" alt="{{ $pressArticle->source }}'s logo" /></div>
             </div>
             <div class="flex_2 flex_column center_content">
@@ -49,5 +82,5 @@
             {{ $press->links() }}
         </article>
     </div>
-</article>
+</article> --}}
 @endsection
