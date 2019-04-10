bfc056c8012e5988fc18b3b5c0df511ebbfe9f85
Esjes <esmeralda160867@hotmail.com>
2019-04-10 10:43:11 +0200

    CHANGE: gallery.scss, photos/index.blade | USERSTORY: bugfixing .cover

--- resources/views/photos/index.blade.php
+++ resources/views/photos/index.blade.php
@@ -3,71 +3,21 @@
 @section('pagetitle', 'All photos')
 
 @section('content')
-
-
-
-
-    <div class="grid_container">
-        <div class="gallery_card">
-            <img src="{{url('/img/devlucht.jpg')}}" alt="">
-            <div class="card_phototitle">Dit is een mooie foto</div>
-            <div class="card_creator">Pietje Puk</div>
-        </div>
-        <div class="gallery_card">
-            <img src="{{url('/img/devlucht.jpg')}}" alt="">
-            <div class="card_phototitle">Dit is een mooie foto</div>
-            <div class="card_creator">Pietje Puk</div>
-        </div>
-        <div class="gallery_card">
-            <img src="{{url('/img/devlucht.jpg')}}" alt="">
-            <div class="card_phototitle">Dit is een mooie foto</div>
-            <div class="card_creator">Pietje Puk</div>
-        </div>
-        <div class="gallery_card">
-            <img src="{{url('/img/devlucht.jpg')}}" alt="">
-            <div class="card_phototitle">Dit is een mooie foto</div>
-            <div class="card_creator">Pietje Puk</div>
-        </div>
-        <div class="gallery_card">
-            <img src="{{url('/img/devlucht.jpg')}}" alt="">
-            <div class="card_phototitle">Dit is een mooie foto</div>
-            <div class="card_creator">Pietje Puk</div>
-        </div>
-    </div>
-
-    @endsection
-
-       {{--<article class="content_container flex_column">
-        <div class="content_positioning">
             <article class="content_wrapper">
-                Success Handlers for User changes
+                {{-- Success Handlers for User changes --}}
                 @include('inc.messages')
             </article>
             <article class="content_wrapper">
                 <a class="button button_act" href="/photos/create">join the contest</a>
             </article>
-            {{-- <article class="content_wrapper">
-                <div class="content_container">
-                    <div class="content_top">
-                        <div class="left">Enkele foto's</div>
-                        <div class="right">Series</div>
-                    </div>     
-                </div>
-            </article>
-        </div>
-        <div class="content_positioning cards_container gallery_card_container">
-        @foreach($photos as $photo)
-            <article class="content_wrapper flex_column card_wrapper gallery_card_wrapper">
-                <section class="content_sub_wrapper">
-                    <p class="h1">{{ $photo->title }}</p>
-                </section>
-                <section class="content_sub_wrapper">
-                    <div><a href="/photos/{{ $photo->id }}"><img class="img_size" src="/storage/{{ $photo->src }}"/></a></div>
-                    <div>{!! nl2br(e( $photo->descr))!!}</div>
-                </section>
+        <div class="grid_container">
+            @foreach($photos as $photo)
+            <article class="gallery_card">
+                    <a href="/photos/{{ $photo->id }}"><img class="cover" src="/storage/{{ $photo->src }}"/></a>
+                    <div class="card_phototitle">{{ $photo->title }}</div>
+                    <div class="card_creator">naam {!! nl2br(e( $photo->descr))!!}</div>
             </article>
             @endforeach
-            <section class="whitespace"></section>
         </div>
         <div class="content_positioning">
             <article class="content_wrapper flex_row">
@@ -75,4 +25,5 @@
             </article>
         </div>
     </article>
-    --}} 
+    
+    @endsection
