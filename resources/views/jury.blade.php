8715a2eaa949b4a668d7780e1d3db814df57aaf0
Esjes <esmeralda160867@hotmail.com>
2019-04-10 11:09:28 +0200

    CHANGE: jury.scss, jury.blade | USERSTORY: bugfixing .cover

--- resources/views/jury.blade.php
+++ resources/views/jury.blade.php
@@ -5,62 +5,27 @@
 @section('content')
 
 <div class="grid_container">
-    <div class="jury_card">
-        <img src="{{url('/img/avatar/default.png')}}" alt="">
-        <div class="card_name">Pietje Puk</div>  
-        <div class="card_title">Beeldredacteur NRC Handelsblad</div>
-        <div class="card_description">Pietjes Puk is al 25 jaar beeldredacteur bij het NRC. Pietje heeft heel veel verstand van foto's. In 1530 was hij de eerste winnaar van de zilveren camera</div>
-    </div>
-    <div class="jury_card">
-        <img src="{{url('/img/avatar/default.png')}}" alt="">
-        <div class="card_name">Pietje Puk</div>  
-        <div class="card_title">Beeldredacteur NRC Handelsblad</div>
-        <div class="card_description">Pietjes Puk is al 25 jaar beeldredacteur bij het NRC. Pietje heeft heel veel verstand van foto's. In 1530 was hij de eerste winnaar van de zilveren camera</div>
-    </div>
-    <div class="jury_card">
-        <img src="{{url('/img/avatar/default.png')}}" alt="">
-        <div class="card_name">Pietje Puk</div> 
-        <div class="card_title">Beeldredacteur NRC Handelsblad</div>
-        <div class="card_description">en als deze tekst anders wordt wat gebeurt er dan met de hoogte van mijn kaartje..Pietjes Puk is al 25 jaar beeldredacteur bij het NRC. Pietje heeft heel veel verstand van foto's. In 1530 was hij de eerste winnaar van de zilveren camera. En wat als het gewoon nog meer tekst heeft? Er mag dus niet meer tekst in komen dan in max 135 height past.</div>
-    </div>
-    <div class="jury_card">
-        <img src="{{url('/img/avatar/default.png')}}" alt="">
-        <div class="card_name">Pietje Puk</div>  
-        <div class="card_title">Beeldredacteur NRC Handelsblad</div>
-        <div class="card_description">Pietjes Puk is al 25 jaar beeldredacteur bij het NRC. Pietje heeft heel veel verstand van foto's. In 1530 was hij de eerste winnaar van de zilveren camera</div>
-    </div>
-    <div class="jury_card">
-        <img src="{{url('/img/avatar/default.png')}}" alt="">
-        <div class="card_name">Pietje Puk</div>  
-        <div class="card_title">Beeldredacteur NRC Handelsblad</div>
-        <div class="card_description">Pietjes Puk is al 25 jaar beeldredacteur bij het NRC. Pietje heeft heel veel verstand van foto's. In 1530 was hij de eerste winnaar van de zilveren camera</div>
-    </div>
+        @foreach($juryMembers as $juryMember)
+        <div class="jury_card">
+            <a href="users/{{ $juryMember->id }}"><img class="cover" src="{{url('/img/avatar/default.png')}}" alt=""></a>
+                <div class="card_name">{{ $juryMember->name }}</div>
+                <div class="card_title">Beeldredacteur NRC Handelsblad</div>
+                <div class="card_description">{!! nl2br(e($juryMember->descr)) !!}</div>
+        </div>      
+        @endforeach
 </div>
 
-{{-- <div class="grid_jury">
-    <div class="jury_card flex_wrap">
-        @foreach($juryMembers as $juryMember)
-            <a href="users/{{ $juryMember->id }}">
-                <img src="{{url('/img/jury1.jpg')}}" alt="">
-                <div class="name">{{ $juryMember->name }}</div>
-                <div class="title">Donateur</div>
-                <div class="description">{!! nl2br(e($juryMember->descr)) !!}</div>
-            </a>
-        @endforeach
-    </div>
-</div> --}}
-
-{{-- <article class="content_container flex_column">
+<article class="content_container flex_column">
     <div class="content_positioning">
         <article class="content_wrapper">
-            {{-- Success Handlers for User changes 
+            Success Handlers for User changes 
             @include('inc.messages')
         </article>
         <article class="content_wrapper">
             <a class="button button_act" href="/photos/create">join the contest</a>
         </article>
     </div>
-    <div class="content_positioning cards_container">
+    {{-- <div class="content_positioning cards_container">
     @foreach($juryMembers as $juryMember)
         <article class="content_wrapper flex_column card_wrapper">
             <section class="content_sub_wrapper">
@@ -70,7 +35,7 @@
                 <div><a href="/sponsors/{{ $juryMember->id }}"><img class="img_size" src="img/avatar/{{ $juryMember->avatar }}"/></a></div>
                 <div class="description">{!! nl2br(e($juryMember->descr)) !!}</div>
                 <div class="name">{{ $juryMember->name }}</div>
-                {{-- <img src="{{url('/img/jury1.jpg')}}" alt=""> 
+                <img src="{{url('/img/jury1.jpg')}}" alt=""> 
             </section>
         </article>
     @endforeach
